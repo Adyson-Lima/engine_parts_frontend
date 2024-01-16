f4152f7459aa9975beb700a0b2837d5287dd7d3c
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-16 16:00:13 -0300

    5° commit - tdd tela Parts: Existe link Novo em Parts? GREEN

--- src/pages/Parts/index.js
+++ src/pages/Parts/index.js
@@ -11,6 +11,8 @@ export default function Parts(){
       </div>
       <div className="card-body">
 
+        <Link data-testid="mylink" className="btn btn-success" style={{marginBottom: '10px'}} to="/newupdate/0">Novo</Link>
+
       </div>
     </div>
   );
