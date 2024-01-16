c17bb4c7db5d41f3e6b7e6cba7da22f2b1971327
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-16 16:44:30 -0300

    23° commit - tdd tela NewUpdate: Existe botão enviar em NewUpdate? GREEN

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -30,6 +30,8 @@ export default function NewUpdate(){
             style={{marginBottom: '20px'}} className="form-control" 
             placeholder="###"></input>
           </div>  
+
+          <button data-testid="btnenviar" type="submit" className="btn btn-primary">Enviar</button>
           
         </form>
 
