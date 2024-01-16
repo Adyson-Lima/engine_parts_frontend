4ddfe32cf59862cc681a26909b27c3079903e799
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-16 15:56:20 -0300

    3° commit - tdd tela Parts: Existe card em Parts? GREEN

---
+++ src/pages/Parts/index.js
@@ -0,0 +1,18 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import api from '../../services/api';
+
+export default function Parts(){
+
+  return(
+    <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
+      <div className="card-header bg-primary" style={{color: '#fff'}}>
+        Parts Crud
+      </div>
+      <div className="card-body">
+
+      </div>
+    </div>
+  );
+
+}
